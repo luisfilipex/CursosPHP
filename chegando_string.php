8369eeefcbedcf64f0f79d9e18a07cc9e029e1e3
Luis Filipe e Silva <137459500+luisfilipex@users.noreply.github.com>
2025-02-18 17:59:02 -0300

    Add files via upload

---
+++ chegando_string.php
@@ -0,0 +1,10 @@
+<?php
+$string = "LUIS";
+$idade = 25;
+$profissao = "programador";
+
+if ($string == "LUIS") {
+    echo "Olá, meu nome é $string, eu tenho $idade anos e sou $profissao.";
+} else {
+    echo "Olá, eu não sou o Luis.";
+}
