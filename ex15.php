6fd50cf5f67bdd140b3a7fbf50e63852b24ee15b
Luis Filipe Silva <filipe_rapex@hotmail.com>
2025-06-07 14:43:28 -0300

    condicao veradeira e falsa if/else

---
+++ ex15.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 4;
+
+if ($a == $b){
+    echo "A igual a b <br>";
+}else if ($a == $c){
+    echo "A igual a c <br>";
+} else {
+    echo "A nao e igual a b nem a c <br>";
+}
