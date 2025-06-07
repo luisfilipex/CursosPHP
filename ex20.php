0691e1d7433127d6eb75b78414cf69b22ee15cc4
Luis Filipe Silva <filipe_rapex@hotmail.com>
2025-06-07 16:24:26 -0300

    operador &&

---
+++ ex20.php
@@ -0,0 +1,13 @@
+<?php
+  if( 5  > 10 && 10 > 5){
+    echo 'Entrou no if 1';
+    }
+  
+  if( 50> 10 && 10>5){
+    echo 'Entrou no if 2';
+  }
+
+  if(50>10 && 10 >500){
+    echo 'Entrou no if 3';
+  }
+
